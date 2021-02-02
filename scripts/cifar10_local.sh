04a1a3da3257f3a252f95c0ecda07b6c344e9983
nsom <Somavarapun@gmail.com>
2021-02-02 08:14:01 -0500

    Format local scripts.

--- scripts/cifar10_local.sh
+++ scripts/cifar10_local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-srun -p short -c 6 --account=overcap --gres=gpu:1 --job-name="test" -x calculon,neo,johnny5,c3po,rosie,hal,walle,bender python src/VisualClassificationRunner.py
+python src/VisualClassificationRunner.py
